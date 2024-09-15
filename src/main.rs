58011b210ecb862e4fe74bb0332f551bcf0c171d
riccardocinti <cintiriccardo@gmail.com>
2024-09-15 16:21:05 +0200

    Main refactoring using clap

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,28 @@
 use std::io;
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+struct Args {
+    #[arg(value_name = "CODE", help = "UTF-8 code to convert")]
+    code: String,
+}
 
 fn main() {
-    println!("*** UTF-8 CONVERTER ***");
-    println!("Insert the UTF-8 code to convert:");
+    if let Err(e) = run(Args::parse()) {
+        eprintln!("{}", e);
+        std::process::exit(1);
+    }
+}
 
-    let mut code = String::new();
+fn run(args: Args) -> io::Result<()> {
+    let code = args.code;
 
-    io::stdin()
-        .read_line(&mut code)
-        .expect("Failed to read line.");
-
-    let without_prefix = code.trim_end().trim_start_matches("0x");
+    let without_prefix = &code.trim_end().trim_start_matches("0x");
     let decimal_code = usize::from_str_radix(without_prefix, 16).expect("Conversion failed");
     let converted_code = utf_converter::convert_to_bytes(decimal_code);
 
     println!("You inserted: {}", code.trim_end());
     println!("The converted bytes result is: {}", converted_code);
+
+    Ok(())
 }
